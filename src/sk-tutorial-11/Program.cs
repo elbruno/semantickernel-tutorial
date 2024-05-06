380ff6870a629dfa77880554d73e3a057e503066
Bruno Capuano <bcapuano@gmail.com>
2024-05-06 16:32:08 -0400

    test with phi3

--- src/sk-tutorial-11/Program.cs
+++ src/sk-tutorial-11/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Planning;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
 using Microsoft.SemanticKernel.Plugins.OpenApi.Extensions;
+using Spectre.Console;
 using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -18,7 +19,13 @@ var apiKey = config["AZURE_OPENAI_APIKEY"];
 
 // Create a chat completion service
 var builder = Kernel.CreateBuilder();
-builder.AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
+//builder.AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
+
+builder.AddOpenAIChatCompletion(
+    modelId: "phi3",
+    endpoint: new Uri("http://w11-eb20asus-docker-desktop:8080"),
+    apiKey: "apikey");
+
 Kernel kernel = builder.Build();
 
 var plugInName = "PetsSearch";
@@ -48,9 +55,7 @@ With the results of the search, show the information for each pet including the 
 Show the result of the pets with super hero names as a indented list in plain text. 
 Do not generate HTML or MARKDOWN, just text.";
 
-// display the goal in the console, with a title GOAL
-Console.WriteLine("GOAL");
-Console.WriteLine(planGoal);
+
 
 // create planner
 var planner = new FunctionCallingStepwisePlanner(
@@ -63,19 +68,77 @@ var planner = new FunctionCallingStepwisePlanner(
 
 var result = await planner.ExecuteAsync(kernel, planGoal);
 
-// Display the plan in the console, with a title PLAN
-Console.WriteLine("PLAN");
+// display the goal in the spectre console, with a title GOAL
+Spectre.Console.AnsiConsole.MarkupLine("[bold green]--------------------------------------------------[/]");
+Spectre.Console.AnsiConsole.MarkupLine("[bold green]GOAL[/]");
+Spectre.Console.AnsiConsole.MarkupLine(planGoal);
+Spectre.Console.AnsiConsole.MarkupLine("[bold green]--------------------------------------------------[/]");
 
-// iterate over the steps in the plan
-foreach (var step in result.ChatHistory)
+// display the final answer in the spectre console, with a title FINAL ANSWER
+Spectre.Console.AnsiConsole.MarkupLine("[bold green]--------------------------------------------------[/]");
+Spectre.Console.AnsiConsole.MarkupLine("[bold green]FINAL ANSWER[/]");
+Spectre.Console.AnsiConsole.MarkupLine(result.FinalAnswer);
+Spectre.Console.AnsiConsole.MarkupLine("[bold green]--------------------------------------------------[/]");
+
+DisplayPlan(result);
+
+void DisplayPlan(FunctionCallingStepwisePlannerResult result)
 {
-    // add line separator
-    Console.WriteLine("--------------------------------------------------");
-    Console.WriteLine($"Role: {step.Role}");
-    Console.WriteLine(step.ToString());
-    Console.WriteLine("--------------------------------------------------");
-}
+    // Create a table
+    var table = new Table();
+    table.Border(TableBorder.Ascii);
 
-// display the final answer in the console, with a title FINAL ANSWER
-Console.WriteLine("FINAL ANSWER");
-Console.WriteLine(result.FinalAnswer);
+    // Add some columns
+    table.AddColumn(new TableColumn("Role").Centered());
+    table.AddColumn(new TableColumn("ModelId").Centered());
+    table.AddColumn(new TableColumn("Type").Centered());
+    table.AddColumn(new TableColumn("Step").Centered());
+
+    // Add some rows
+    foreach (var step in result.ChatHistory)
+    {
+        var row = new List<Spectre.Console.Rendering.Renderable>
+        {
+            new Markup($"[bold]{step.Role}[/]"),
+            new Markup($"[bold]{step.ModelId}[/]")
+        };
+
+        var stepType = "";
+        var lines = new List<string>();
+        // get the last item from step.Items
+        var line = step.Items.LastOrDefault();
+
+        if (line is TextContent)
+        {
+            var currentLine = line as TextContent;
+            stepType = "TextContent";
+
+            // if currentLine.Text lenght is longer than 250 characters, get the 1st 250 characters
+            if (currentLine.Text.Length > 250)
+                lines.Add(currentLine.Text.Substring(0, 250)+ " ...");
+            else
+                lines.Add(currentLine.Text);
+        }
+        else if (line is FunctionCallContent)
+        {
+            var currLine = line as FunctionCallContent;
+            stepType = "FunctionCallContent";
+            lines.Add($"PlugIn Name: {currLine.PluginName} {Environment.NewLine} Function Name :{currLine.FunctionName}");
+        }
+
+
+        row.Add(new Markup($"[bold]{stepType}[/]"));
+
+        // create a single string containing all the elements in the variable lines
+        lines.Add(Environment.NewLine);
+        var rowString = string.Join(Environment.NewLine, lines);
+        row.Add(new Text(rowString));
+
+        table.AddRow(row);
+
+    }
+
+
+
+    AnsiConsole.Write(table);
+}
