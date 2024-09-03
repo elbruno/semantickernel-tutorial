a719af2e983e14fc2b253cad7b6570c4d098d970
Bruno Capuano <bcapuano@gmail.com>
2024-09-03 15:31:44 -0400

    Update model path and chat completion service config
    Changed modelPath to "D:\phi3\models\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32" in Program.cs.
    Uncommented AddOnnxRuntimeGenAIChatCompletion method.
    Commented out AddOpenAIChatCompletion method.

--- src/LabsPhi3_sk_rag02/Program.cs
+++ src/LabsPhi3_sk_rag02/Program.cs
@@ -58,16 +58,15 @@ SpectreConsoleOutput.DisplayTitleH3("1st approach will be to ask the question di
 SpectreConsoleOutput.DisplayTitleH3("2nd approach will be to add facts to a semantic memory and ask the question again");
 Console.WriteLine("");
 
-//var modelPath = @"D:\phi3\models\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32";
-var modelPath = @"d:\phi3\models\Phi-3.5-mini-instruct-onnx-web";
+var modelPath = @"D:\phi3\models\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32";
 
 // Create a chat completion service
 var builder = Kernel.CreateBuilder();
-//builder.AddOnnxRuntimeGenAIChatCompletion(modelPath: modelPath);
-builder.AddOpenAIChatCompletion(
-    modelId: "phi3.5",
-    endpoint: new Uri("http://localhost:11434"),
-    apiKey: "apikey");
+builder.AddOnnxRuntimeGenAIChatCompletion(modelPath: modelPath);
+//builder.AddOpenAIChatCompletion(
+//    modelId: "phi3.5",
+//    endpoint: new Uri("http://localhost:11434"),
+//    apiKey: "apikey");
 builder.AddLocalTextEmbeddingGeneration();
 Kernel kernel = builder.Build();
 var chat = kernel.GetRequiredService<IChatCompletionService>();
